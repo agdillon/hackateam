4ce2725634d77bb256f5f1813905d76eabf0a234
Christa Sparks <christa@Christas-MacBook-Pro.local>
2018-10-29 14:31:45 -0600

    Add events get one and get all routes

---
+++ routes/events.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+const knex = require('../knex')
+// READ ALL records for this table
+router.get('/', (req, res, next) => {
+    knex('events')
+      .then((rows) => {
+        res.json(rows)
+      })
+      .catch((err) => {
+        next(err)
+      })
+  })
+// READ ONE record for this table
+router.get('/:id', (req, res, next) => {
+    knex('events')
+      .where('id',req.params.id)
+      .then((rows) => {
+        res.json(rows)
+      })
+      .catch((err) => {
+        next(err)
+      })
+  })
+
+module.exports = router
