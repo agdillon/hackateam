27dc80b8231c74d36c8e9d16e77b28f5401ca19f
Christa Sparks <christa@Christas-MacBook-Pro.local>
2018-10-28 19:21:34 -0600

    Create basic teams routes

--- routes/teams.js
+++ routes/teams.js
@@ -1,24 +1,88 @@
 const express = require('express')
 const router = express.Router()
 const knex = require('../knex')
+const uuidv4 = require('uuid/v4')
 // READ ALL records for this table
+
 router.get('/', (req, res, next) => {
-  res.send('ALL RECORDS')
-})
+    knex('teams')
+      .then((rows) => {
+        res.json(rows)
+      })
+      .catch((err) => {
+        next(err)
+      })
+  })
 // READ ONE record for this table
 router.get('/:id', (req, res, next) => {
-  res.send('ONE RECORD')
-})
+    knex('teams')
+      .where('id',req.params.id)
+      .then((rows) => {
+        res.json(rows)
+      })
+      .catch((err) => {
+        next(err)
+      })
+  })
 // CREATE ONE record for this table
 router.post('/', (req, res, next) => {
-  res.send('CREATED RECORD')
-})
+    knex('teams')
+      .insert({
+        "key": uuidv4(),
+        "team_size_limit": req.body.team_size_limit,
+        "event_id": req.body.event_id,
+        "has_idea": req.body.idea,
+        "is_full": req.body.is_full,
+        "description": req.body.description
+      })
+      .returning('*')
+      .then((data) => {
+        res.json(data[0])
+      })
+      .catch((err) => {
+        next(err)
+      })
+  })
 // UPDATE ONE record for this table
 router.put('/:id', (req, res, next) => {
-  res.send('UPDATED RECORD')
-})
+    knex('teams')
+    .where('id', req.params.id)
+    .then((data) => {
+      knex('teams')
+      .where('id', req.params.id)
+      .limit(1)
+      .update({
+        "team_size_limit": req.body.team_size_limit,
+        "event_id": req.body.event_id,
+        "has_idea": req.body.idea,
+        "is_full": req.body.is_full,
+        "description": req.body.description
+      })
+      .returning('*')
+      .then((data) => {
+        res.json(data[0])
+      })
+    })
+    .catch((err) => {
+      next(err)
+    })
+  })
 // DELETE ONE record for this table
-router.delete('/:id', (req, res, next) => {
-  res.send('DELETED RECORD')
-})
+router.delete('/:id', function(req, res, next) {
+    knex('teams')
+      .where('id', req.params.id)
+      .first()
+      .then((row) => {
+        if(!row) return next()
+        knex('teams')
+          .del()
+          .where('id', req.params.id)
+          .then(() => {
+            res.send(`ID ${req.params.id} Deleted`)
+          })
+      })
+      .catch((err) => {
+        next(err)
+      })
+  }) 
 module.exports = router
