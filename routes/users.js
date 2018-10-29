45bf7e3dca82aa39e8a9e46da3654ca0b17c3a74
Amanda Dillon <agdillon@gmail.com>
2018-10-29 16:26:25 -0600

    get more user data from github

--- routes/users.js
+++ routes/users.js
@@ -6,17 +6,30 @@ const router = express.Router()
 
 // after successful OAuth login
 router.get('/login', (req, res, next) => {
+  // split name from GitHub to prepopulate first and last name
+  let firstName, lastName
+  let fullName = req.user.displayName
+  let firstSpaceIndex = fullName.indexOf(' ')
+  if (firstSpaceIndex === -1) {
+    firstName = fullName
+  }
+  else {
+    firstName = fullName.slice(0, firstSpaceIndex)
+    lastName = fullName.slice(firstSpaceIndex + 1)
+  }
+
   // check db to see if user already exists (by email in req.user)
-  // I don't actually know what req.user looks like, req.user.email might not be right
   knex('users').first().where('email', req.user.email)
     .then(user => {
       // if so, redirect to dashboard
       if (user) {
-        res.redirect('/dashboard.html')
+        res.send(user)
+        // res.redirect('/dashboard.html')
       }
       // if not, make a db entry for them, redirect to create profile page
       else {
-        knex('users').insert({ key: uuidv4(), email: req.user.email })
+        knex('users').insert({ key: uuidv4(), email: req.user.email,
+          first_name: firstName, last_name: lastName, user_picture_url: req.user.user_picture_url })
           .then(() => {
             res.redirect('/user-profile.html')
           })
@@ -26,10 +39,10 @@ router.get('/login', (req, res, next) => {
 
 // user edit (update)
 router.put('/users/:id', (req, res, next) => {
-  // check authorization -- should also check secret and check if cookie session id is the same as req.params.id
-  if (!req.cookies || req.cookies.expire < new Date()) {
-    next({ status: 401, message: "Not authorized" })
-  }
+  // // check authorization -- should also check secret and check if cookie session id is the same as req.params.id
+  // if (!req.cookies || req.cookies.expire < new Date()) {
+  //   next({ status: 401, message: "Not authorized" })
+  // }
 
   let { first_name, last_name, email, portfolio_url } = req.body
   knex('users').update({ first_name, last_name, email, portfolio_url }).where('id', req.params.id)
@@ -40,10 +53,10 @@ router.put('/users/:id', (req, res, next) => {
 
 // user delete
 router.delete('/users/:id', (req, res, next) => {
-  // check authorization -- should also check secret and check if cookie session id is the same as req.params.id
-  if (!req.cookies || req.cookies.expire < new Date()) {
-    next({ status: 401, message: "Not authorized" })
-  }
+  // // check authorization -- should also check secret and check if cookie session id is the same as req.params.id
+  // if (!req.cookies || req.cookies.expire < new Date()) {
+  //   next({ status: 401, message: "Not authorized" })
+  // }
 
   knex('users').del().where('id', req.params.id)
     .then(() => {
