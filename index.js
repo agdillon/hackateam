f7fa35934319b5024026561d6ae064bcab97ae7d
Amanda Dillon <agdillon@gmail.com>
2018-10-28 21:59:52 -0600

    add users routes and OAuth

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
 
 const port = process.env.PORT || 3000
 const app = express()
@@ -6,6 +7,7 @@ const app = express()
 const usersRouter = require('./routes/users')
 
 app.use(express.json())
+app.use(cookieParser(process.env.COOKIE_SECRET))
 
 // routers
 app.use('/users', usersRouter)
