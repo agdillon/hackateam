e543c0be818ce38f97fcfb7caaffdccd406c6b8b
Christa Sparks <christa@Christas-MacBook-Pro.local>
2018-10-29 11:14:33 -0600

    Send users info associated with team and team info for get 1 team

--- routes/teams.js
+++ routes/teams.js
@@ -5,6 +5,7 @@ const uuidv4 = require('uuid/v4')
 // READ ALL records for this table
 
 router.get('/', (req, res, next) => {
+    // if time pass on skills_wanted for a filter or filter in the route
     knex('teams')
       .then((rows) => {
         res.json(rows)
@@ -14,19 +15,29 @@ router.get('/', (req, res, next) => {
       })
   })
 // READ ONE record for this table
+// Get team info and all associated users
 router.get('/:id', (req, res, next) => {
-    knex('teams')
-      .where('id',req.params.id)
-      .then((rows) => {
-        res.json(rows)
-        //   knex.select('*').from('teams').join('user_team', 'teams.id', 'user_team.team_id')
-        //   .then((data) => {
-        //     res.json(data)
-        //   })
-      })
-      .catch((err) => {
+    knex('user_team')
+    .select('user_team.user_id', 'user_team.team_id', 'users.first_name', 'users.last_name', 'users.email', 'users.portfolio_url')
+    .join('users', 'user_team.user_id', 'users.id')
+    .where('user_team.team_id', req.params.id)
+    .then((userData) => {
+        knex('teams')
+        .select('id', 'team_size_limit', 'has_idea', 'is_full', 'event_id', 'description')
+        .where('id',req.params.id)
+        .then((teamData) => {
+            res.json({
+                teamData: teamData,
+                userData: userData
+            })
+        })
+        .catch((err) => {
+            next(err)
+        })
+    })
+    .catch((err) => {
         next(err)
-      })
+    })
   })
 // CREATE ONE record for this table
 router.post('/', (req, res, next) => {
